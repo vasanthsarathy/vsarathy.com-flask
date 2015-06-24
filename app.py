684b8499521f2387e445d7e2c44f069db6b2bc45
Vasanth Sarathy <vsarathy@gmail.com>
2015-06-24 17:35:34 -0400

    added sitemap and 404

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ app.config['FREEZER_DESTINATION'] = 'gh-pages'
 app.config['FREEZER_DESTINATION_IGNORE'] = ['.git*', 'CNAME', '.gitignore', 'readme.md']
 app.config['FREEZER_RELATIVE_URLS'] = True
 
+
+HOSTING_DOMAIN = 'http://vsarathy.com/'
+
 #controllers
 @app.route("/")
 def index():
@@ -16,6 +19,23 @@ def index():
 @app.route('/research.html')
 def contact():
     return render_template('research.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route('/404.html')
+def static_404():
+    return render_template('404.html')
+
+@app.route('/sitemap.xml')
+def generate_sitemap():
+    locations = [
+        ('',              '2015-06-24'),
+    ]
+    sites = [(HOSTING_DOMAIN + l[0], l[1]) for l in locations]
+    return render_template('sitemap.xml', sites=sites)
+
 
 #launch
 if __name__ == "__main__":
